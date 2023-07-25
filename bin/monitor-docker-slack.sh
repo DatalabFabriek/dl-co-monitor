8390e48cbc2b822867b30733e77699149dd94ddd
Harmen van der Veer <harmen_github_com@xtremesf.nl>
2023-07-25 17:39:07 +0200

    Bash... I hate you

--- bin/monitor-docker-slack.sh
+++ bin/monitor-docker-slack.sh
@@ -1,10 +1,11 @@
-#!/bin/bash -ex
+#!/bin/bash
+echo "Starting Docker Services monitor..."
 
 if [ -f /run/secrets/dl-co-monitor-slack-webhook ]; then
        export SLACK_WEBHOOK=$(cat /run/secrets/dl-co-monitor-slack-webhook)
 fi
 
-if [$STARTUP_DELAY -gt 0]; then
+if [ "$STARTUP_DELAY" -gt "0" ]; then
        echo "Entering start-up delay mode for $STARTUP_DELAY seconds..."
        sleep $STARTUP_DELAY
 fi
@@ -12,5 +13,7 @@ fi
 python /datalab/monitor-docker-slack.py  --check_interval "$CHECK_INTERVAL" \
        --slack_webhook "$SLACK_WEBHOOK" --whitelist "$WHITE_LIST" \
        --msg_prefix "$MSG_PREFIX"
+
+echo "Docker Services monitor stopped"
 
 ## File : monitor-docker-slack.sh ends
