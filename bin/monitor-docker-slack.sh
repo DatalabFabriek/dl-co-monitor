f82c9ac2b9d3f25945c257ab388be9722f969d83
Harmen van der Veer <harmen_github_com@xtremesf.nl>
2023-07-26 13:43:27 +0200

    Additional logging (incl time)

--- bin/monitor-docker-slack.sh
+++ bin/monitor-docker-slack.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-echo "Starting Docker Services monitor..."
+echo "[$(date +'%Y-%m-%d %H:%M:%S.%N')]\tStarting Docker Services monitor..."
 
 if [ -f /run/secrets/dl-co-monitor-slack-webhook ]; then
        export SLACK_WEBHOOK=$(cat /run/secrets/dl-co-monitor-slack-webhook)
 fi
 
 if [ ${STARTUP_DELAY:=0} -gt 0 ]; then
-       echo "Entering start-up delay mode for $STARTUP_DELAY seconds..."
+       echo "[$(date +'%Y-%m-%d %H:%M:%S.%N')]\tEntering start-up delay mode for $STARTUP_DELAY seconds..."
        sleep $STARTUP_DELAY
 fi
 
-echo "Starting monitoring script..."
+echo "[$(date +'%Y-%m-%d %H:%M:%S.%N')]\tStarting monitoring script..."
 
 python /datalab/monitor-docker-slack.py  --check_interval "$CHECK_INTERVAL" \
        --slack_webhook "$SLACK_WEBHOOK" --whitelist "$WHITE_LIST" \
        --msg_prefix "$MSG_PREFIX"
 
-echo "Docker Services monitor stopped"
+echo "[$(date +'%Y-%m-%d %H:%M:%S.%N')]\tDocker Services monitor stopped"
 
 ## File : monitor-docker-slack.sh ends
