33766b359a3a94d31be3603ae7c13a667a8aa5e0
Harmen van der Veer <harmen_github_com@xtremesf.nl>
2023-07-26 13:51:49 +0200

    Added tz

--- bin/monitor-docker-slack.sh
+++ bin/monitor-docker-slack.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-echo "[$(date +'%Y-%m-%d %H:%M:%S.%N')]\tStarting Docker Services monitor..."
+TZ='Europe/Amsterdam'
+
+echo -e "[$(date +'%Y-%m-%d %H:%M:%S.%N')]\tStarting Docker Services monitor..."
 
 if [ -f /run/secrets/dl-co-monitor-slack-webhook ]; then
        export SLACK_WEBHOOK=$(cat /run/secrets/dl-co-monitor-slack-webhook)
 fi
 
 if [ ${STARTUP_DELAY:=0} -gt 0 ]; then
-       echo "[$(date +'%Y-%m-%d %H:%M:%S.%N')]\tEntering start-up delay mode for $STARTUP_DELAY seconds..."
+       echo -e "[$(date +'%Y-%m-%d %H:%M:%S.%N')]\tEntering start-up delay mode for $STARTUP_DELAY seconds..."
        sleep $STARTUP_DELAY
 fi
 
-echo "[$(date +'%Y-%m-%d %H:%M:%S.%N')]\tStarting monitoring script..."
+echo -e "[$(date +'%Y-%m-%d %H:%M:%S.%N')]\tStarting monitoring script..."
 
 python /datalab/monitor-docker-slack.py  --check_interval "$CHECK_INTERVAL" \
        --slack_webhook "$SLACK_WEBHOOK" --whitelist "$WHITE_LIST" \
        --msg_prefix "$MSG_PREFIX"
 
-echo "[$(date +'%Y-%m-%d %H:%M:%S.%N')]\tDocker Services monitor stopped"
+echo -e "[$(date +'%Y-%m-%d %H:%M:%S.%N')]\tDocker Services monitor stopped"
 
 ## File : monitor-docker-slack.sh ends
